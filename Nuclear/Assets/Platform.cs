5ef73a4c21fa7a46779ab84a959afbc843851075
RobertoFigueroa <35494933+RobertoFigueroa@users.noreply.github.com>
2019-04-18 12:41:59 -0600

    Commit inicial
    Este commit contiene la estructura básica de las plataformas (se crean inifinitamente).

---
+++ Nuclear/Assets/Platform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private PatformManager _platformManager;
+
+    private void OnEnable()
+    {
+        _platformManager = GameObject.FindObjectOfType<PatformManager>();   
+    }
+    private void OnBecameInvisible()
+    {
+        //recicla la plataforma
+        _platformManager.RecyclerPlatform(this.gameObject);
+    }
+}
---
+++ Nuclear/Assets/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody _player;
+    public float force = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        _player.AddForce(0,0,force);
+    }
+
+    private void Jump()
+    {
+        if (_player)
+        {
+            if(Mathf.Abs(_player.velocity.y) < 0.0005f)
+            {
+                _player.AddForce(0,force,0);
+            }
+        }
+    }
+}
