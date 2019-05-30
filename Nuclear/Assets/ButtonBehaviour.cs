93072e97f9c2c84a7629e3c58af5efb3a2d0342f
RobertoFigueroa <35494933+RobertoFigueroa@users.noreply.github.com>
2019-05-29 18:22:37 -0600

    Mod. Main menu
    Agregué sonido y otras opciones al menu

---
+++ Nuclear/Assets/ButtonBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonBehaviour : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler
+{
+
+
+    public AudioSource sound1;
+    public AudioSource sound2;
+
+
+    public void OnPointerEnter(PointerEventData e)
+    {
+        if (sound1) sound1.Play();
+    }
+    public void OnPointerDown(PointerEventData e)
+    {
+        if (sound2) sound2.Play();
+    }
+}
---
+++ Nuclear/Assets/MainMenuCanvas.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuCanvas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    public void ShowPanel(string panelName)
+    {
+        transform.Find(panelName).gameObject.SetActive(true);
+    }
+
+    public void HidePanel(string panelName)
+    {
+        transform.Find(panelName).gameObject.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
